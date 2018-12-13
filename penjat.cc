a6a0bec70f4f34b07aa01445cd0bef8d6d15c9e4
Iridum <30082001+Iridum@users.noreply.github.com>
2018-12-13 09:18:44 +0100

    Update penjat.cc

--- penjat.cc
+++ penjat.cc
@@ -7,15 +7,19 @@
 #include <string>
 #include <vector>
 
+#include <algorithm>
+
 using namespace std;
 
- 
+
+
+
 int random(int min, int max) {   
     srand(time(NULL));
     return rand() % (max - min) + min;
 }
 
-string get_random_word() {
+string getRandomWord() {
 	ifstream fin("paraules.txt");
 	int x;
 	fin >> x;
@@ -30,11 +34,52 @@ string get_random_word() {
 
 	fin.close();
 	
-	return s;
+	return paraula;
+}
+
+string getPlayerSprite(int errors){
+	string sprite = "0033333330\n0020000040\n0020000050\n0020000768\n0020000060\n002000090a\n1111100000";
+	for(int i = 0; i < 11; i++){
+		bool showPart = i <= errors;
+		switch(i){
+			case 0:
+				replace( sprite.begin(), sprite.end(), '0', ' ');
+				break;
+			case 1:
+				replace( sprite.begin(), sprite.end(), '1', showPart ? '-' : ' ');
+				break;
+			case 2:
+				replace( sprite.begin(), sprite.end(), '2', showPart ? '|' : ' ');
+				break;
+			case 3:
+				replace( sprite.begin(), sprite.end(), '3', showPart ? '-' : ' ');
+				break;
+			case 4:
+				replace( sprite.begin(), sprite.end(), '4', showPart ? '|' : ' ');
+				break;
+			case 5:
+				replace( sprite.begin(), sprite.end(), '5', showPart ? 'o' : ' ');
+				break;
+			case 6:
+				replace( sprite.begin(), sprite.end(), '6', showPart ? '|' : ' ');
+				break;
+			case 7:
+				replace( sprite.begin(), sprite.end(), '7', showPart ? '/' : ' ');
+				break;
+			case 8:
+				replace( sprite.begin(), sprite.end(), '8', showPart ? '\\' : ' ');
+				break;
+			case 9:
+				replace( sprite.begin(), sprite.end(), '9', showPart ? '/' : ' ');
+				break;
+			case 10:
+				replace( sprite.begin(), sprite.end(), 'a', showPart ? '\\' : ' ');
+				break;
+		}
+	}
+	
+	return sprite;
 }
 
 int main() {
-	while(true){
-		
-	}
 }
