d6b68e1866c98a2c910238eaf0c0e9509ee04508
Iridum <30082001+Iridum@users.noreply.github.com>
2018-12-20 09:50:41 +0100

    Yasta
    jeje

--- penjat.cc
+++ penjat.cc
@@ -4,6 +4,7 @@
 #include <fstream>
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -11,12 +12,28 @@
 
 using namespace std;
 
-
+////////////////////////////////
+//          BALENA            //
+////////////////////////////////
+/*******************************
+ *                             *
+ *         .                   *
+ *        ":"                  *
+ *      ___:____     |"\/"|    *
+ *    ,'        `.    \  /     *
+ *    |  O        \___/  |     *
+ *  ~^~^~^~^~^~^~^~^~^~^~^~^~  *
+ *                             *
+ *******************************/
+ 
+ 
+// Funció que retorna un nombre aleatori R tal que: min =< R =< max
 int random(int min, int max) {   
     srand(time(NULL));
     return rand() % (max - min) + min;
 }
 
+// Dona una paraula aleatoria del arxiu paraules.txt
 string getRandomWord() {
 	ifstream fin("paraules.txt");
 	int x;
@@ -35,6 +52,7 @@ string getRandomWord() {
 	return paraula;
 }
 
+// Crea un dibuix del nino segons el nombre d'errors
 string getPlayerSprite(int errors){
 	string sprite = "0033333330\n0020000040\n0020000050\n0020000768\n0020000060\n002000090a\n1111100000";
 	for(int i = 0; i < 11; i++){
@@ -55,6 +73,7 @@ string getPlayerSprite(int errors){
 	return sprite;
 }
 
+// Oculta la paraula canviant les lletres per '_' si l'usuari no ha trobat la lletra
 vector<char> hideWord(string word, vector<char> currentChars){
 	vector<char> solved;
 	for(unsigned int i = 0; i < word.length(); i++){
@@ -68,6 +87,7 @@ vector<char> hideWord(string word, vector<char> currentChars){
 	return solved;
 }
 
+// Torna TRUE si la paraula conté la lletra, si no, torna FALSE
 bool hasChar(string word, char guess){
 	bool trobat = false;
 	
@@ -77,6 +97,7 @@ bool hasChar(string word, char guess){
 	return trobat;
 }
 
+// Dibuixa la interficie del joc
 void drawUI(vector<char> hideWord, vector<char> currentChars, int errors, string message, int status){
 	cout << "------------------------------------------------------------------------------" << endl;
 	cout << "Paraula: " << endl;
@@ -99,41 +120,72 @@ void drawUI(vector<char> hideWord, vector<char> currentChars, int errors, string
 	cout << message << endl;
 	cout << endl;
 	cout << "------------------------------------------------------------------------------" << endl;
-	if(status == 0)
-		cout << "Dona'm una lletra minúscula: ";
-	else
-		cout << "Vols tornar a jugar? (S/N): ";
+	if(status == 0) cout << "Dona'm una lletra minúscula: ";
+	else cout << "Vols tornar a jugar? (S/N): ";
 }
 
+// Logica del joc i inici.
 int main() {
-	string jugar = "s";
-	while(jugar == "s" or jugar == "S"){
+	string jugar = "!";
+	int v = 0;
+	int d = 0;
+	do {
 		string word = getRandomWord();
 		vector<char> currentChars;
 		vector<char> hiddenWord;
 		int errors = 0;
 		bool acabat = false;
+		string missatge = "";
+		
 		while(not acabat){
 			hiddenWord = hideWord(word, currentChars);
 			char actual;
 			bool lletraValida = true;
-			string missatge = "";
 			do{
 				drawUI(hiddenWord, currentChars, errors, missatge, 0);
-				cin >> actual;
+				if(cin >> actual) {} else acabat = true; // Per alguna misteriosa rao el if not no funciona.
 				
-			}while(lletraValida);
+				if(actual > 96 and actual < 123) lletraValida = true;
+				else{
+					missatge = "El caracter introduit no es valid.";
+					lletraValida = false;
+				}
+				
+				for(unsigned int i = 0; i < currentChars.size() and lletraValida; i++){
+					if(currentChars[i] == actual){
+						lletraValida = false;
+						missatge = "Ja has fet servir aquesta lletra.";
+					}
+				}
+			}while(not lletraValida);
 			currentChars.push_back(actual);
-			if(not hasChar(word, actual)) errors++;
-			if(errors == 10) acabat = true;
-			
+			if(not hasChar(word, actual)){
+				missatge = "La lletra no esta";
+				errors++;
+			}else missatge = "";
+			if(errors == 10){
+				 acabat = true;
+				 d++;
+			}
 			bool trobat = false;
+			hiddenWord = hideWord(word, currentChars);
 			for(unsigned int i = 0; i < hiddenWord.size() and not trobat; i++)
 				if(hiddenWord[i] == '_') trobat = true;
-			if(not trobat) acabat = true;
+			if(not trobat){
+				acabat = true;
+				v++;
+			}
 		}
+		ostringstream vss;
+		vss << v; 
+		string vs = vss.str();
 		
-		drawUI(hiddenWord, currentChars, errors, "", 1);
-		cin >> jugar;
-	}
+		ostringstream dss;
+		dss << d;
+		string ds = dss.str();
+		// c++ es tan estupid que no deixa sumar 2 literals. Perque? Ningú ho sap
+		missatge = (errors == 10 ? "Ets dolentissim desinstala el joc buuuuh. Victories " : "Bestia champion ets un maquina!!!!!!!!!!!!!!!!!! Victories ")+ vs + " | Derrotes: " + ds;
+		drawUI(hiddenWord, currentChars, errors, missatge, 1);
+		if(cin >> jugar) {} else jugar = "!";  // Per alguna misteriosa rao el if not no funciona.
+	}while(jugar == "s" or jugar == "S");
 }
